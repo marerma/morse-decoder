2d295e63eb9cc9e804b1a167c821cfe4ed5320c3
Maria Ermolaeva <maria.kadantseva@gmail.com>
2022-07-20 22:32:32 +0300

    feat: solved morse

--- src/index.js
+++ src/index.js
@@ -37,8 +37,45 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
+  MORSE_TABLE['**********'] = ' '
+ 
+  let decodedWord
+  
+  const arrayDigit = []
+  let aArray = Array.from(expr)
+  
+  while (aArray.length != 0) {
+    arrayDigit.push(aArray.splice(0, 10))
+  }
+  
+  let letter = []
+  arrayDigit.forEach(function(item) {
+    letter.push(item.join(''))})
+  
+  let dotsWord = []
+    for (let j = 0; j < letter.length; j++) {
+     let str = letter[j]
+     str.includes('10')? str = str.replace(/10/g, '.') : str
+     str.includes('11')? str = str.replace(/11/g, '-') : str
+     str.includes('0')? str = str.replace(/0/g, '') : str
+     dotsWord.push(str) 
+  }
+  
+  let result = []
+  dotsWord.map((item) => {
+    for (let symbol in MORSE_TABLE) {
+      s = MORSE_TABLE[symbol] 
+      if (item === symbol) {
+       result.push(s)
+      
+    } 
+  }
+  })
+  
+decodedWord = result.join('')
+return decodedWord
 }
 
 module.exports = {
